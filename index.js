4b69e3ea67d40b6fa8f824f5f9426bc78b288a38
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-08-31 14:35:29 +0530

    Update image styling and add links.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  window.location.href = "/#popup1";
-});
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   window.location.href = "/#popup1";
+// });
+
+
+// ! utitility functions below
+// ! if they are still here
+// ! it probably means I forgot to delete them
+
+document.getElementById('behave').addEventListener('click', (event) => {
+
+  const imgs = document.getElementsByClassName('img');
+
+  for( var i=0; i<imgs.length; i++) {
+
+    imgs.item(i).classList.add('remove-border-radius');
+  }
+
+  window.alert('After clicking okay, images would not have curved edges!!');
+})
