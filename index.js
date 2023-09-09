79e87e067e7f2500977aa76d2c5e8a1cddb16a34
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-09-10 00:59:04 +0530

    Add titles

--- index.js
+++ index.js
@@ -19,3 +19,103 @@
 
 //   window.alert('After clicking okay, images would not have curved edges!!');
 // })
+
+
+// ! test code
+
+// th-01
+document.getElementById('th-01-tx').addEventListener('mouseover', event => {
+
+    document.getElementById('th-01').style.filter = "blur(2px) brightness(0.7)";
+})
+
+document.getElementById('th-01').addEventListener('mouseleave', event => {
+
+    document.getElementById('th-01').style.filter = "blur(0px) brightness(1)";
+})
+
+document.getElementById('th-01').addEventListener('mouseover', event => {
+
+    document.getElementById('th-01').style.filter = "blur(2px) brightness(0.7)";
+})
+
+// th-02
+document.getElementById('th-02-tx').addEventListener('mouseover', event => {
+
+    document.getElementById('th-02').style.filter = "blur(3px) brightness(0.7)";
+})
+
+document.getElementById('th-02').addEventListener('mouseleave', event => {
+
+    document.getElementById('th-02').style.filter = "blur(0px) brightness(1)";
+})
+
+document.getElementById('th-02').addEventListener('mouseover', event => {
+
+    document.getElementById('th-02').style.filter = "blur(3px) brightness(0.7)";
+})
+
+// th-03
+document.getElementById('th-03-tx').addEventListener('mouseover', event => {
+
+    document.getElementById('th-03').style.filter = "blur(5px) brightness(0.7)";
+})
+
+document.getElementById('th-03').addEventListener('mouseleave', event => {
+
+    document.getElementById('th-03').style.filter = "blur(0px) brightness(1)";
+})
+
+document.getElementById('th-03').addEventListener('mouseover', event => {
+
+    document.getElementById('th-03').style.filter = "blur(5px) brightness(0.7)";
+})
+
+// th-04
+document.getElementById('th-04-tx').addEventListener('mouseover', event => {
+
+    document.getElementById('th-04').style.filter = "blur(3px) brightness(0.7)";
+})
+
+document.getElementById('th-04').addEventListener('mouseleave', event => {
+
+    document.getElementById('th-04').style.filter = "blur(0px) brightness(1)";
+})
+
+document.getElementById('th-04').addEventListener('mouseover', event => {
+
+    document.getElementById('th-04').style.filter = "blur(3px) brightness(0.7)";
+})
+
+// th-05
+document.getElementById('th-05-tx').addEventListener('mouseover', event => {
+
+    document.getElementById('th-05').style.filter = "blur(3px) brightness(0.7)";
+})
+
+document.getElementById('th-05').addEventListener('mouseleave', event => {
+
+    document.getElementById('th-05').style.filter = "blur(0px) brightness(1)";
+})
+
+document.getElementById('th-05').addEventListener('mouseover', event => {
+
+    document.getElementById('th-05').style.filter = "blur(3px) brightness(0.7)";
+})
+
+
+// th-06
+document.getElementById('th-06-tx').addEventListener('mouseover', event => {
+
+    document.getElementById('th-06').style.filter = "blur(3px) brightness(0.7)";
+})
+
+document.getElementById('th-06').addEventListener('mouseleave', event => {
+
+    document.getElementById('th-06').style.filter = "blur(0px) brightness(1)";
+})
+
+document.getElementById('th-06').addEventListener('mouseover', event => {
+
+    document.getElementById('th-06').style.filter = "blur(3px) brightness(0.7)";
+})
