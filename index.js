b6cc87576a98720cfb7603f5bf451079e26aacad
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-09-09 22:32:03 +0530

    Start development mode.

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  window.location.href = "./#popup1";
-});
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   window.location.href = "./#popup1";
+// });
 
 
 // ! utitility functions below
 // ! if they are still here
 // ! it probably means I forgot to delete them
+// ! but it does not matter
 
-document.getElementById('behave').addEventListener('click', (event) => {
+// document.getElementById('behave').addEventListener('click', (event) => {
 
-  const imgs = document.getElementsByClassName('img');
+//   const imgs = document.getElementsByClassName('img');
 
-  for( var i=0; i<imgs.length; i++) {
+//   for( var i=0; i<imgs.length; i++) {
 
-    imgs.item(i).classList.add('remove-border-radius');
-  }
+//     imgs.item(i).classList.add('remove-border-radius');
+//   }
 
-  window.alert('After clicking okay, images would not have curved edges!!');
-})
+//   window.alert('After clicking okay, images would not have curved edges!!');
+// })
